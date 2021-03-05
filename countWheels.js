26cb29c664e7624266f85ae70bdc1cee0cd65ef5
CryptoThibault <thibault.chalaoux@gmail.com>
2021-03-05 15:07:02 +0100

    exos

--- countWheels.js
+++ countWheels.js
@@ -1,5 +1,9 @@
+const NB_WHEELS_MONOCYCLE = 1
+const NB_WHEELS_MOTO = 2
+const NB_WHEELS_CAR = 4
+const NB_WHEELS_LIMO = 6
 function countWheels(nb_monocycle, nb_moto, nb_voiture, nb_limousine) {
-  return nb_monocycle + nb_moto * 2 + nb_voiture * 4 + nb_limousine * 6
+  return nb_monocycle * NB_WHEELS_MONOCYCLE + nb_moto * NB_WHEELS_MOTO + nb_voiture * NB_WHEELS_CAR + nb_limousine * NB_WHEELS_LIMO
 }
 
 console.log(`Le total de roues est ${countWheels(3, 6, 4, 2)}`)
